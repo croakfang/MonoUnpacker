d43fc088ec2d5c39e65832bb021e3db02fcb62c2
1605296300@qq.com <long123456789>
2022-08-09 22:24:40 +0800

    fix read error

--- UmaMonoUnpacker.py
+++ UmaMonoUnpacker.py
@@ -14,14 +14,14 @@ for obj in env.objects:
         if obj.serialized_type.nodes:
             tree = obj.read_typetree()
             
-            with open("tmp.txt", "w+",encoding='utf-8') as jsonFile:
+            with open("tmp.json", "w+",encoding='utf-8') as jsonFile:
                 jsonFile.write(json.dumps(tree, indent = 4))
                 jsonFile.close();
 
-            os.startfile("tmp.txt")
+            os.startfile("tmp.json")
             input("修改后按任意键继续"); 
 
-            with open("tmp.txt", "w+",encoding='utf-8') as jsonFile:
+            with open("tmp.json", "r",encoding='utf-8') as jsonFile:
                 tree = json.load(jsonFile)
                 jsonFile.close();
             obj.save_typetree(tree)
